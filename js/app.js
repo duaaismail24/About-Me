22c6319d73f3f127c0e9ae643f81eeb21ab61663
duaaismail24 <doaaee7@gmail.com>
2019-11-11 17:19:50 +0200

    about me

---
+++ js/app.js
@@ -0,0 +1,83 @@
+'use strict';
+var money = prompt('Do you like money?');
+console.log(money);
+money = money.toLowerCase(); 
+switch (money) {
+    case 'yes':
+    case 'y':
+        
+    alert('Oh, who don\'t!');
+break;
+    case 'No':
+    case 'n':
+     alert('Seriously !!');
+     break;
+     default:
+     alert("Answer with yes or no please");
+}
+var studing = prompt('Do you like studing?');
+console.log(studing);
+studing = studing.toLowerCase(); 
+switch (studing) {
+    case 'yes':
+    case 'y':
+        
+    alert('Oh, we have a Nerd here hhh');
+break;
+    case 'No':
+    case 'n':
+     alert('I\'am too !!');
+     break;
+     default:
+     alert("Answer with yes or no please");
+}
+var hoppy = prompt('Do you like Online games?');
+console.log(hoppy);
+hoppy = hoppy.toLowerCase(); 
+switch (hoppy) {
+    case 'yes':
+    case 'y':
+        
+    alert('Great , join us :O');
+break;
+    case 'No':
+    case 'n':
+     alert('COMMON MAN !!');
+     break;
+     default:
+     alert("Answer with yes or no please");
+}
+
+var active = prompt('Are you lazy person?');
+console.log(active);
+active = active.toLowerCase(); 
+switch (active) {
+    case 'yes':
+    case 'y':
+        
+    alert('Go to Gym now!');
+break;
+    case 'No':
+    case 'n':
+     alert('awesome');
+     break;
+     default:
+     alert("Answer with yes or no please");
+}
+
+var level = prompt('Did you fishe your Bachelor\' degree?');
+console.log(level);
+level = level.toLowerCase(); 
+switch (level) {
+    case 'yes':
+    case 'y':
+        
+    alert('Cool');
+break;
+    case 'No':
+    case 'n':
+     alert('Keep Going');
+     break;
+     default:
+     alert("Answer with yes or no please");
+}
